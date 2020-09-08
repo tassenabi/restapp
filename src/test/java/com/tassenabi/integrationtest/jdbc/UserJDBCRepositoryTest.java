e113d3507d6a04025ca532a73f80551cca9f4d47
tassenabi <abi.tassen@posteo.de>
2020-09-08 11:35:57 +0300

    ..

--- src/test/java/com/tassenabi/integrationtest/jdbc/UserJDBCRepositoryTest.java
+++ src/test/java/com/tassenabi/integrationtest/jdbc/UserJDBCRepositoryTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
